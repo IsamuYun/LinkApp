930fae71bb26a84893c14981a2f3c8a523bd8e48
IsamuYun <yunchunnan@qq.com>
2020-02-18 09:17:53 -0800

    0218 Commit

--- src/socket/ws.js
+++ src/socket/ws.js
@@ -6,14 +6,14 @@ YellowBox.ignoreWarnings([
     "Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?"
 ]);
 
-// const SERVER_URL = 'ws://192.168.0.174:5000';
+const SERVER_URL = 'ws://192.168.0.174:5000';
 // const SERVER_URL = 'ws://157.245.124.194:5000';
-const SERVER_URL = 'ws://172.31.99.189:5000';
+// const SERVER_URL = 'ws://172.31.99.189:5000';
 
 export default class WS {
+    static BASE_URL = "http://192.168.0.174:5000/";
     // static BASE_URL = "http://157.245.124.194:5000/";
-    // static BASE_URL = "http://192.168.0.174:5000/";
-    static BASE_URL = "http://172.31.99.189:5000/";
+    // static BASE_URL = "http://172.31.99.189:5000/";
     
     static init() {
         this.ws = io(SERVER_URL, { 
